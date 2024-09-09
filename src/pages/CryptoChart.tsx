687af9cbe6423c11d0bb09931fe72087cf2c13c8
Nikolay-Nikiforuk <user@MacBook-Pro-user.local>
2024-09-09 18:53:16 +0300

    cryptocurrency-chart(page): changed styles for main, added new styles for header and footer

--- src/pages/CryptoChart.tsx
+++ src/pages/CryptoChart.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+import './CryptoChart.css';
+
 export default function CryptoChart() {
   return (
     <>
       <Header />
       <main className="main">
-        <div className="chart">
+        <div className="container">
           <h1>Chart</h1>
         </div>
       </main>
